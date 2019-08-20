6cabcea0975961b29e8650e615a9bbcb1e0dbff5
Lili Boxer <ninaliboxer@gmail.com>
2019-08-20 13:21:30 -0700

    statically rendered emoji component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import Emoji from './Emoji';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return <Emoji emoji="😀" name="grinning face"/>;
 }
   
--- src/components/Emoji.js
+++ src/components/Emoji.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Emoji() {
-  
+function Emoji({ emoji, name }) {
+  return (
+    <>
+      <p>{emoji}</p>
+      <h4>{name}</h4>
+    </>
+  );
 }
 
 Emoji.propTypes = {
-
+  emoji: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired
 };
 
 export default Emoji;
